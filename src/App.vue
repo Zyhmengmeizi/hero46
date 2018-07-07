ab68d159e9fd35c54b1ecd3d71d036fef9362604
zhangyuehua <630274693@qq.com>
2018-07-07 08:52:31 +0800

    头部与侧边栏提取

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<template>
+<div>
+ <!-- 头部 -->
+ <app-header></app-header>
+ <app-sidebar></app-sidebar>
+</div>
+</template>
+
+<script>
+ // 导入header组件
+ import AppHeader from './components/AppHeader.vue';
+ import AppSidebar from './components/AppSidebar.vue';
+  // ES6到出模块的语法
+export default {
+  name: 'app',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  },
+    components:{
+      AppHeader,
+      AppSidebar
+    }
+}
+// webpack支持各种模块化的语法 CommonJS AMD ES6
+// module.exports = {
+
+// }
+</script>
+
+<style>
+
+</style>
